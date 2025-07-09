ca3c685fc21447975f374c14d07f713615756d03
tnehezd <tarczaynehez.dora@csfk.org>
2025-07-09 12:16:11 +0200

    add more md-s

--- docs/conf.py
+++ docs/conf.py
@@ -50,6 +50,8 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+master_doc = 'index' # Így kell lennie!
+
 
 # --- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
