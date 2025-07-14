df1c63325482f8f560d66240f43d892aec0f0d33
tnehezd <tarczaynehez.dora@csfk.org>
2025-07-14 18:34:34 +0200

    remove things from example

--- example/MESA_grid/rm_things.sh
+++
@@ -1,7 +0,0 @@
-for d in run_*MSUN_z*; do
-  if [ -d "$d" ]; then # Csak akkor folytatjuk, ha valóban egy létező mappa
-    echo "Törlés a(z) $d mappában..."
-    rm -rf "$d/photos" "$d/make" "$d/gyre_output"
-  fi
-done
-echo "Kész."
